aa5e0d7f4b71fe29fd8cdbb24cd428643361e3ef
Ashutoshy115 <ashutoshy115work@gmail.com>
2026-07-12 12:20:06 +0100

    fix: address review feedback and Sonar S5838 in ApplyModuleSubCommandTest

--- src/test/java/com/seed4j/cli/command/infrastructure/primary/ApplyModuleSubCommandTest.java
+++ src/test/java/com/seed4j/cli/command/infrastructure/primary/ApplyModuleSubCommandTest.java
@@ -25,20 +25,16 @@ class ApplyModuleSubCommandTest {
   void shouldBuildCommandSpecWithPropertyWithoutCandidates() {
     Seed4JModulesApplicationService modules = mock(Seed4JModulesApplicationService.class);
     ProjectsApplicationService projects = mock(ProjectsApplicationService.class);
-
     Seed4JModuleResource module = mock(Seed4JModuleResource.class);
     when(module.slug()).thenReturn(new Seed4JModuleSlug("test-module"));
-
     Seed4JModuleApiDoc apiDoc = mock(Seed4JModuleApiDoc.class);
     when(apiDoc.operation()).thenReturn(new Seed4JModuleOperation("Test operation"));
     when(module.apiDoc()).thenReturn(apiDoc);
-
     Seed4JModulePropertyDefinition property = mock(Seed4JModulePropertyDefinition.class);
     when(property.key()).thenReturn(new Seed4JPropertyKey("unknownProperty"));
     when(property.type()).thenReturn(Seed4JPropertyType.STRING);
     when(property.isMandatory()).thenReturn(false);
     when(property.defaultValue()).thenReturn(Optional.empty());
-
     Seed4JModulePropertiesDefinition properties = mock(Seed4JModulePropertiesDefinition.class);
     when(properties.stream()).thenAnswer(i -> Stream.of(property));
     when(module.propertiesDefinition()).thenReturn(properties);
@@ -47,6 +43,6 @@ class ApplyModuleSubCommandTest {
     CommandSpec spec = command.commandSpec();
 
     assertThat(spec).isNotNull();
-    assertThat(spec.findOption("unknown-property").description()[0]).isEqualTo("");
+    assertThat(spec.findOption("unknown-property").description()[0]).isEmpty();
   }
 }
