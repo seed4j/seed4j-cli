ee35ecb66cbbc72907616c3c9848ba4e3d78c2bd
Renan Franca <renan.andradefranca@gmail.com>
2026-07-21 11:49:31 -0300

    fix(command): correct coverage exclusion reason typo

--- src/main/java/com/seed4j/cli/command/infrastructure/primary/Seed4JCommandRunner.java
+++ src/main/java/com/seed4j/cli/command/infrastructure/primary/Seed4JCommandRunner.java
@@ -17,14 +17,14 @@ class Seed4JCommandRunner implements CommandLineRunner, ExitCodeGenerator {
   }
 
   @Override
-  @ExcludeFromGeneratedCodeCoverage(reason = "Don not need to test when using picocli framework")
+  @ExcludeFromGeneratedCodeCoverage(reason = "Do not need to test when using picocli framework")
   public void run(String... args) {
     CommandLine commandLine = new CommandLine(command.buildCommandSpec());
     exitCode = commandLine.execute(args);
   }
 
   @Override
-  @ExcludeFromGeneratedCodeCoverage(reason = "Don not need to test when using picocli framework")
+  @ExcludeFromGeneratedCodeCoverage(reason = "Do not need to test when using picocli framework")
   public int getExitCode() {
     return exitCode;
   }
